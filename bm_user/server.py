838b8a2a5351231d651e472c27840c60e71f34f9
Per Lindstrand <fryingpanjoe@gmail.com>
2014-02-11 00:59:41 +0100

    WIP

--- bm_user/server.py
+++ bm_user/server.py
@@ -23,23 +23,3 @@ class HttpServer(object):
             return json.dumps({'access_token': token})
         else:
             return json.dumps({'status': 'login failed'})
-
-
-class HttpServer(RPCServer):
-
-    def __init__(self, store):
-        self.store = store
-
-    @expose('add_user')
-    def add_user(self, name, password, email):
-        user_id = self.store.add_user(name, password, email)
-        if user_id:
-            expire_time = int(time.time() + TOKEN_TTL)
-            token_hash = hashlib.sha1()
-            token_hash.update(name)
-            token_hash.update(email)
-            token_hash.update(expire_time)
-            token = '%s:%s:%s' % (user_id, expire_time, token_hash.hexdigest())
-            return json.dumps({'access_token': token})
-        else:
-            return json.dumps({'status': 'login failed'})
--- bm_user/store.py
+++ bm_user/store.py
@@ -2,12 +2,12 @@ import sqlite3
 from contextlib import closing
 import crypt
 
-DEFAULT_DATABASE = 'bm_account.db'
+DEFAULT_DATABASE = 'bm_user.db'
 MEMORY_DATABASE = ':memory:'
 
-USER_TABLE_SCRIPT = """create table bm_account_users(
+USER_TABLE_SCRIPT = """create table bm_user(
         user_id integer primary key autoincrement
-    ,   name text unique
+    ,   username text unique
     ,   password text
     ,   email text unique
     ,   suspended integer default 0
@@ -16,7 +16,7 @@ USER_TABLE_SCRIPT = """create table bm_account_users(
 );"""
 
 
-class AccountStore(object):
+class UserStore(object):
 
     def __init__(self, filename=DEFAULT_DATABASE):
         self.filename = filename
@@ -29,13 +29,13 @@ class AccountStore(object):
     def close(self):
         self.db.close()
 
-    def add_user(self, name, raw_password, email):
+    def add_user(self, username, raw_password, email):
         encrypted_password = crypt.encrypt_password(raw_password)
         with closing(self.db.cursor()) as cur:
             try:
                 cur.execute(
-                    'insert into bm_account_users (name, password, email) values (?,?,?)',
-                    (name, encrypted_password, email,))
+                    'insert into bm_account_users (username, password, email) values (?,?,?)',
+                    (username, encrypted_password, email,))
                 cur.execute('select last_insert_rowid()')
                 user_id = cur.fetchone()[0]
             except sqlite3.DatabaseError:
@@ -45,12 +45,12 @@ class AccountStore(object):
                 self.db.commit()
                 return user_id
 
-    def delete_user(self, user_id):
+    def delete_user(self, user_id, deleted=True):
         with closing(self.db.cursor()) as cur:
             try:
                 cur.execute(
-                    'update bm_account_users set deleted = 1 where user_id = ?',
-                    (user_id,))
+                    'update bm_account_users set deleted = ? where user_id = ?',
+                    (1 if deleted else 0, user_id,))
             except sqlite3.DatabaseError:
                 self.db.rollback()
                 return False
@@ -58,12 +58,26 @@ class AccountStore(object):
                 self.db.commit()
                 return True
 
-    def authenticate_user(self, name, raw_password):
+    def suspend_user(self, user_id, suspended=True):
         with closing(self.db.cursor()) as cur:
             try:
                 cur.execute(
-                    'select user_id, password from bm_account_users where name = ? limit 1',
-                    (name,))
+                    'update bm_account_users set suspended = ? where user_id = ?',
+                    (1 if suspended else 0, user_id,))
+            except sqlite3.DatabaseError:
+                self.db.rollback()
+                return False
+            else:
+                self.db.commit()
+                return True
+
+    def authenticate_user(self, username, raw_password):
+        with closing(self.db.cursor()) as cur:
+            try:
+                cur.execute(
+                    'select user_id, password from bm_account_users where' +
+                    ' username = ? and suspended = 0 and deleted = 0 limit 1',
+                    (username,))
             except sqlite3.DatabaseError:
                 pass
             else:
